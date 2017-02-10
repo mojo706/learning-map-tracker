39a3aacfd1424ba0610430495b7e0b50092c537b
chuchu <xriseugene@gmail.com>
2017-02-10 15:37:52 +0300

    Add the methods 'skills_studied' and 'skills_not_studied' Add: Sets to make it easier to get a the difference between 'skills_studied' and 'skills_not_studied'

--- learningmap.py
+++ learningmap.py
@@ -1,8 +1,9 @@
 class LearningMap(object):
 
-  def __init__(self, user): # initilize the class
+  def __init__(self, user):
     self.user = user
     self.my_skills = []
+    self.studied_skills = []
     
   def add_skills(self):
 
@@ -14,6 +15,29 @@ class LearningMap(object):
       count += 1
         
   def view_skills(self):
-    for i,skill in enumerate(self.my_skills): # Added the enumerate function to get the index and value from my_skills
-      print '%d. %s' % (i+1, skill) # Add formatters for better output
+    for i,skill in enumerate(self.my_skills): # Loop through my_skills getting index and 
+      print '%d. %s' % (i+1, skill) # Added String formatters
+      
+  def skills_studied(self):
+    studied_skills = []
+    value2 = 'Yes'
+    
+    while value2 == "Yes":
+      value1 = raw_input("Enter skill completed:")
+      self.studied_skills.append(value1)
+      value2 = raw_input("Any other skill? (Yes or No)" )
+      print (studied_skills)
+      
+  def skills_not_studied(self):
+    setMySkills = set(self.my_skills)
+    setStudiedSkills = set(self.studied_skills)
+    set_skills_not_studied = setMySkills - setStudiedSkills
+    list_skills_not_studied = list(set_skills_not_studied) 
+     for i,skill in enumerate(elf.):
+      print '%d. %s' % (i+1, skill)
+    
+
+
+
+
 
