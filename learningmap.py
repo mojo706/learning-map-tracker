441095cf71ab9fd6fa539a8d6bf6fd17b4296a9e
chuchu <xriseugene@gmail.com>
2017-02-10 14:55:06 +0300

    Fix 'add_skills()' to make the 'my_skills' list global Fix 'view_skills()' to display all skills in a better format

--- learningmap.py
+++ learningmap.py
@@ -1,12 +1,12 @@
 class LearningMap(object):
     def __init__(self, user):
-        self.name = name
-        
+        self.user = user
+        my_skills=[]
 
-    def add_skills(self, value):
-        global my_skills = []
+    def add_skills(self):
         count  = 0
-        while count <= 10:
+        value = ''
+        while count <= 5:
             value = raw_input("Add a skill:")
             my_skills.append(value)
             count += 1
@@ -14,3 +14,4 @@ class LearningMap(object):
     def view_skills(self):
         for i,skill in enumerate(my_skills):
             return (i+1, skill)
+
