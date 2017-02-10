fbafb2f3cf05e072e51ef31f0cb11a62bf4a2896
chuchu <xriseugene@gmail.com>
2017-02-10 15:16:37 +0300

    Fix issue with my_skills where it was local to 'add_skills' by adding it to '__init__()' to make it globally available Add formatters to make the output of 'view_skills()' more readable

--- learningmap.py
+++ learningmap.py
@@ -1,17 +1,19 @@
 class LearningMap(object):
-    def __init__(self, user):
-        self.user = user
-        my_skills=[]
 
-    def add_skills(self):
-        count  = 0
-        value = ''
-        while count <= 5:
-            value = raw_input("Add a skill:")
-            my_skills.append(value)
-            count += 1
+  def __init__(self, user): # initilize the class
+    self.user = user
+    self.my_skills = []
+    
+  def add_skills(self):
+
+    count  = 0
+    value = ''
+    while count <= 4:
+      value = raw_input("Add a skill:")
+      self.my_skills.append(value)
+      count += 1
         
-    def view_skills(self):
-        for i,skill in enumerate(my_skills):
-            return (i+1, skill)
+  def view_skills(self):
+    for i,skill in enumerate(self.my_skills): # Added the enumerate function to get the index and value from my_skills
+      print '%d. %s' % (i+1, skill) # Add formatters for better output
 
